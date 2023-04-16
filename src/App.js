a0ac1d9b0d6e84af8e5cfb40f584a59a116e227b
borednuna <hanunshaka02@gmail.com>
2023-04-17 05:51:19 +0700

    feat: finalize banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './infrastructure/components/header/header';
+import Banner from './infrastructure/components/banner/banner';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
