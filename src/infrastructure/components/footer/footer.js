fb5b613e709dd1664cac61cf4694e1b23e7a9c49
borednuna <hanunshaka02@gmail.com>
2023-04-18 22:57:53 +0700

    feat: finalize footer

--- src/infrastructure/components/footer/footer.js
+++ src/infrastructure/components/footer/footer.js
@@ -1,10 +1,45 @@
 import React from "react";
 import Theme from '../../../application/globals/theme'
+import { ThemeProvider } from '@mui/material/styles';
 import "./footer.scss";
+
+import IconButton from '@mui/material/IconButton';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
     return(
-        <div className="footer"></div>
+        <ThemeProvider theme={Theme}>
+            <div className="footer">
+                <div className='logo'>
+                    <div className="text">
+                        <h1 id='nusantara'>Nusantara</h1>
+                        <h1 id='store'>Store</h1>
+                    </div>
+                </div>
+
+                <div className="logos">
+                    <IconButton aria-label="hobby" size="large" color="white">
+                        <InstagramIcon fontSize="large"/>
+                    </IconButton>
+
+                    <IconButton aria-label="hobby" size="large" color="white">
+                        <GitHubIcon fontSize="large"/>
+                    </IconButton>
+
+                    <IconButton aria-label="hobby" size="large" color="white">
+                        <LinkedInIcon fontSize="large"/>
+                    </IconButton>
+                </div>
+
+                <div className="contact">
+                    <p>About</p>
+                    <p>FAQ</p>
+                    <p>Feedback</p>
+                </div>
+            </div>
+        </ThemeProvider>
     )
 }
 
