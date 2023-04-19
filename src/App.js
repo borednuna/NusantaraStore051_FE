799d8876661b208923a38081aefdb17ea5640bf1
borednuna <hanunshaka02@gmail.com>
2023-04-20 00:11:55 +0700

    feat: finalize gallery component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './infrastructure/components/header/header';
 import Footer from './infrastructure/components/footer/footer';
+import Gallery from './infrastructure/components/gallery/gallery';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Gallery />
       <Footer />
     </div>
   );
---
+++ src/infrastructure/components/gallery/gallery.js
@@ -0,0 +1,57 @@
+import React from "react";
+import './gallery.scss'
+import { useRef, useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
+
+import image1 from '../../../presentation/resources/jaizer-capangpangan-2Xhv5zrTLN0-unsplash.jpg';
+import image2 from '../../../presentation/resources/mocno-fotografia-nzYcN7Vz9BI-unsplash.jpg';
+import image3 from '../../../presentation/resources/omar-prestwich-mBjrF0MK6mc-unsplash.jpg';
+import image4 from '../../../presentation/resources/road-trip-with-raj-rYOSMioTHXg-unsplash.jpg';
+
+register();
+
+const Gallery = () => {
+    const swiperElRef = useRef(null);
+
+    useEffect(() => {
+        // listen for Swiper events using addEventListener
+        swiperElRef.current.addEventListener('progress', (e) => {
+        // eslint-disable-next-line
+          const [swiper, progress] = e.detail;
+          console.log(progress);
+        });
+    
+        swiperElRef.current.addEventListener('slidechange', (e) => {
+          console.log('slide changed');
+        });
+      }, []);
+
+    return(
+        <div className="gallery">
+            <swiper-container
+                ref={swiperElRef}
+                slides-per-view="1"
+                navigation="true"
+                pagination="true"
+                loop="true"
+                className="my-swiper"
+                autoplay="true"
+            >
+                <swiper-slide>
+                    <img src={image1} alt="image1" />
+                </swiper-slide>
+                <swiper-slide>
+                    <img src={image2} alt="image2" />
+                </swiper-slide>
+                <swiper-slide>
+                    <img src={image3} alt="image3" />
+                </swiper-slide>
+                <swiper-slide>
+                    <img src={image4} alt="image4" />
+                </swiper-slide>
+            </swiper-container>
+        </div>
+    )
+}
+
+export default Gallery;
