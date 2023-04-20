fa50865235881b9039abc2c35e0948d8e7287b34
borednuna <hanunshaka02@gmail.com>
2023-04-20 07:15:16 +0700

    feat: finalize review card and refactor seller_card

---
+++ src/infrastructure/components/reviews_card/reviews_card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import './reviews_card.scss';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import StarIcon from '@mui/icons-material/Star';
+
+const ReviewsCard = () => {
+    return(
+        <div className="reviewscard">
+            <h1>Reviews(50)</h1>
+            <div className="individual-cards">
+                <AccountCircleIcon fontSize="large" sx={{margin: '1rem'}} />
+                <div className="text">
+                    <h2>Bunda Corla</h2>
+                    <h3><StarIcon fontSize="small" sx={{color: '#FFD600'}} />3.5</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+
+            <div className="individual-cards">
+                <AccountCircleIcon fontSize="large" sx={{margin: '1rem'}} />
+                <div className="text">
+                    <h2>Bunda Corla</h2>
+                    <h3><StarIcon fontSize="small" sx={{color: '#FFD600'}} />3.5</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+
+            <div className="individual-cards">
+                <AccountCircleIcon fontSize="large" sx={{margin: '1rem'}} />
+                <div className="text">
+                    <h2>Bunda Corla</h2>
+                    <h3><StarIcon fontSize="small" sx={{color: '#FFD600'}} />3.5</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        <p id="load">Load More</p>
+        </div>
+    )
+}
+
+export default ReviewsCard;
