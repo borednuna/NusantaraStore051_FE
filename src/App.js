e67b04df286a0331ef1f1867c7cfd94e3aa84178
borednuna <hanunshaka02@gmail.com>
2023-04-21 23:01:33 +0700

    feat: finalize feedback page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.scss';
 import Header from './infrastructure/components/header/header';
 import Footer from './infrastructure/components/footer/footer';
 import ChatFab from './infrastructure/components/chat_fab/chat_fab';
-import About from './presentation/pages/about/about';
+import Feedback from './presentation/pages/feedback/feedback';
 
 function App() {
   return (
     <div className="App">
       <ChatFab />
       <Header />
-      <About />
+      <Feedback />
       <Footer />
     </div>
   );
