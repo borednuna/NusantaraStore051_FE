5add19778ee5e04bee7a00434bf006d26e56ba1a
borednuna <hanunshaka02@gmail.com>
2023-04-21 21:18:06 +0700

    feat: finalize about page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.scss';
 import Header from './infrastructure/components/header/header';
 import Footer from './infrastructure/components/footer/footer';
 import ChatFab from './infrastructure/components/chat_fab/chat_fab';
-import ProfilePage from './presentation/pages/profile_page/profile_page';
+import About from './presentation/pages/about/about';
 
 function App() {
   return (
     <div className="App">
       <ChatFab />
       <Header />
-      <ProfilePage />
+      <About />
       <Footer />
     </div>
   );
